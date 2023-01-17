7ac8687c184c9337485af38ee15b23516bf8e9c9
bralash <emmanuelasaber@gmail.com>
2023-01-17 10:36:38 +0000

    Working on custom slider

--- src/components/range-slider/range-slider.tsx
+++ src/components/range-slider/range-slider.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React from 'react';
 import styles from './range-slider.module.scss';
 
 export interface RangeSliderProps {
@@ -5,7 +7,9 @@ export interface RangeSliderProps {
 }
 
 export const RangeSlider = ({ className }: RangeSliderProps) => {
-    return <div className={`${styles.rangeSlider} ${className}`}>
-        <input type='range' />
-    </div>;
+    return (
+        <div className={`${styles.rangeSlider} ${className}`}>
+            <input type="range" min="0" max="100" value="50" onChange={()=>{}} className="slider" />
+        </div>
+    );
 };
